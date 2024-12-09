f9e38ee33699c56d46b338ed68d72409ca348885
Siva <siva@rectuoso.co.uk>
2024-12-09 13:02:37 +0530

    UI Updates

--- src/EmailSenderGrid.jsx
+++ src/EmailSenderGrid.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Button, Input, message, Popconfirm, Table, Tag } from "antd";
 import { CheckCircleOutlined, CloseCircleOutlined, DeleteOutlined } from "@ant-design/icons";
 import PropTypes from "prop-types";
@@ -149,12 +149,12 @@ const EmailSenderGrid = ({ apiServer, apiKey }) => {
       key: "domainVerified",
       render: (verified) =>
         verified ? (
-          <Tag icon={<CheckCircleOutlined />} color="darkgreen">
-            VERIFIED
+          <Tag icon={<CheckCircleOutlined />} style={{borderRadius:'15px'}} color="#11a75c">
+            Verified
           </Tag>
         ) : (
-          <Tag icon={<CloseCircleOutlined />} color="darkred">
-            NOT VERIFIED
+          <Tag icon={<CloseCircleOutlined />} style={{borderRadius:'15px'}} color="#ab0a00">
+            Not Verified
           </Tag>
         ),
     },
@@ -164,12 +164,10 @@ const EmailSenderGrid = ({ apiServer, apiKey }) => {
       key: "verified",
       render: (verified) =>
         verified ? (
-          <Tag icon={<CheckCircleOutlined />} color="darkgreen">
-            VERIFIED
+          <Tag icon={<CheckCircleOutlined />} style={{borderRadius:'15px'}} color="#11a75c">Verified
           </Tag>
         ) : (
-          <Tag icon={<CloseCircleOutlined />} color="darkred">
-            NOT VERIFIED
+          <Tag icon={<CloseCircleOutlined />} style={{borderRadius:'15px'}} color="#ab0a00">Not Verified
           </Tag>
         ),
     },
